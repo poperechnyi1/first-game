0e8bc94576a8a8cd351e2227b3e4552ff341a491
spoperechnyi <poperechnyi1@gmail.com>
2020-02-05 22:42:46 +0200

    add-typescript

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SettingPage from './components/SettingsPage/SettingsPage'
+
+const App = () => {
+  return (
+    <div className="App">
+      <SettingPage></SettingPage>
+    </div>
+  );
+}
+
+export default App;
