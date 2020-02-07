4bf7054863a8bbb2e5959dbb92035044f3c1d712
Poperechnyi Stepan <spoperechnyi@exadel.net>
2020-02-07 17:02:29 +0200

    use class base component

--- src/components/SettingsPage/SettingsPage.tsx
+++ src/components/SettingsPage/SettingsPage.tsx
@@ -6,71 +6,101 @@ import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import Button from "@material-ui/core/Button";
 
-const useStyles = makeStyles(theme => ({
-  button: {
-    display: "block",
-    marginTop: theme.spacing(2)
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120
-  }
-}));
 
-export default function SettingPage (){
-
-     const classes = useStyles();
-     const [foundation, setFoundation] = React.useState(2);
-     const [open, setOpen] = React.useState(false);
-
-     //TODO remove any
-     const handleChange = (event: any) => {
-        setFoundation(event.target.value);
-     };
-
-     const handleClose = () => {
-       setOpen(false);
-     };
-
-     const handleOpen = () => {
-       setOpen(true);
-     };
-
-     const handleStart = () => {
-         console.log(foundation);
-     }
-
-    return (
-      <div>
-        <div>
-          <FormControl className={classes.formControl}>
-            <InputLabel id="demo-controlled-open-select-label">
-              Generate field
-            </InputLabel>
-            <Select
-              labelId="demo-controlled-open-select-label"
-              id="demo-controlled-open-select"
-              open={open}
-              onClose={handleClose}
-              onOpen={handleOpen}
-              value={foundation}
-              onChange={handleChange}
-            >
-              <MenuItem value={2}>2</MenuItem>
-              <MenuItem value={3}>3</MenuItem>
-              <MenuItem value={4}>4</MenuItem>
-              <MenuItem value={5}>5</MenuItem>
-              <MenuItem value={6}>6</MenuItem>
-              <MenuItem value={7}>7</MenuItem>
-              <MenuItem value={8}>8</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-        <div>
-          <Button variant="contained" color="primary" onClick={handleStart}>
-            Start Game
-          </Button>
-        </div>
-      </div>
-    );
+interface ISettingState {
+  foundation: number;
+  open: boolean;
 }
+
+export default class SettingPage extends React.Component {
+                 state: ISettingState = {
+                   foundation: 0,
+                   open: false
+                 };
+
+                //  useStyles = makeStyles(theme => ({
+                //    button: {
+                //      display: "block",
+                //      marginTop: theme.spacing(2)
+                //    },
+                //    formControl: {
+                //      margin: theme.spacing(1),
+                //      minWidth: 120
+                //    }
+                //  }));
+
+                //  classes = this.useStyles();
+
+                 setFoundation(value: number): void {
+                   this.setState(state => {
+                     // Important: read `state` instead of `this.state` when updating.
+                     return { foundation: value };
+                   });
+                 }
+
+                 setOpen(isOpen: boolean): void {
+                   this.setState(state => {
+                     return { open: isOpen };
+                   });
+                 }
+
+                 //TODO remove any
+                 handleChange = (event: any) => {
+                   this.setFoundation(event.target.value);
+                 };
+
+                 handleClose = () => {
+                   this.setOpen(false);
+                 };
+
+                 handleOpen = () => {
+                   this.setOpen(true);
+                 };
+
+                 handleStart = () => {
+                   console.log(this.state.foundation);
+                 };
+
+                 render() {
+                   return (
+                     <div>
+                       <div>
+
+                         {/* //FIX problem with classes */}
+                         {/* this.classes.formControl */}
+                         <FormControl>
+                           <InputLabel id="demo-controlled-open-select-label">
+                             Generate field
+                           </InputLabel>
+                           <Select
+                             labelId="demo-controlled-open-select-label"
+                             id="demo-controlled-open-select"
+                             open={this.state.open}
+                             onClose={this.handleClose}
+                             onOpen={this.handleOpen}
+                             value={this.state.foundation}
+                             onChange={this.handleChange}
+                           >
+                             <MenuItem value={2}>2</MenuItem>
+                             <MenuItem value={3}>3</MenuItem>
+                             <MenuItem value={4}>4</MenuItem>
+                             <MenuItem value={5}>5</MenuItem>
+                             <MenuItem value={6}>6</MenuItem>
+                             <MenuItem value={7}>7</MenuItem>
+                             <MenuItem value={8}>8</MenuItem>
+                           </Select>
+                         </FormControl>
+                       </div>
+                       <div>
+                         <Button
+                           variant="contained"
+                           color="primary"
+                           onClick={this.handleStart}
+                         >
+                           Start Game
+                         </Button>
+                       </div>
+                     </div>
+                   );
+                 }
+               }
--- src/redux/GameStore.tsx
+++ src/redux/GameStore.tsx
@@ -1,3 +1,3 @@
-import { createStore } from "redux";
+export default class SomeClass {
 
-let gameProcessing = createStore();
+}
