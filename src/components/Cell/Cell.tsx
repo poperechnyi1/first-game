b63efe6c8316a207547524bdface90ae3bc73e03
spoperechnyi <poperechnyi1@gmail.com>
2020-02-09 12:11:55 +0200

    generate field

--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import './Cell.css';
 
 export default class Cell extends React.Component {
 
     render(){
         return (
-            <div>
+            <div className="Cell">
             <Button>
-                Cell
             </Button>
             </div>
         )
--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -5,9 +5,26 @@ import Cell from '../Cell/Cell';
 
 class Field extends React.Component {
 
+    generateField(){
+        let field = [];
+        for(var i = 0; i<8;i++){
+            let row = [];
+            for(var j = 0; j<8; j++){
+                row.push(<Cell key={`${i},${j}`}/>)
+            }
+        field.push(<div className="row">{row}</div>)
+        }
+
+        return (
+            <div>
+                {field}
+            </div>
+        )
+    }
+
     render(){
         return (
-            <div><Cell/></div>
+            <div>{this.generateField()}</div>
         );
     }
 
