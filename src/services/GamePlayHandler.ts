5a99954de6827c037eac3c0b6b0c36611df1a7ad
spoperechnyi <poperechnyi1@gmail.com>
2020-02-10 22:27:20 +0200

    improve gameplay

--- src/services/GamePlayHandler.ts
+++ src/services/GamePlayHandler.ts
@@ -3,6 +3,20 @@ import { ICell } from "../interfaces/Matrix";
 export default class GamePlayHandler {
   findWinner() {}
 
+  takeCell(
+    matrix: Array<Array<ICell>>,
+    isFirstPlayer: boolean,
+    hPointer: number,
+    vPointer: number
+  ): Array<Array<ICell>> {
+    if (!matrix[hPointer][vPointer].isCellTaken) {
+      matrix[hPointer][vPointer].isCellTaken = true;
+      matrix[hPointer][vPointer].takenBy = isFirstPlayer ? 1 : 2;
+    }
+
+    return matrix;
+  }
+
   generateMatrix(foundation: number): Array<Array<ICell>> {
     const matrix: Array<Array<ICell>> = [];
     for (var i = 0; i < foundation; i++) {
