6f5bf79a6c4dc2d379784c66386a649ef6386791
Poperechnyi Stepan <spoperechnyi@exadel.net>
2020-02-11 19:29:59 +0200

    handle gameplay

--- src/redux/reducers/FieldReducer.tsx
+++ src/redux/reducers/FieldReducer.tsx
@@ -1,18 +1,42 @@
 import { IMatrix } from "../../interfaces/Matrix";
 
 const initialState: IMatrix = {
-  matrix: []
+  matrix: [],
+  firstPlayerSequences: [], //save a map for every subSequences
+  secondPlayerSequences: [],
+  takenAmountOfCells: 1,
+  isGameFinished: false,
 };
 
 export default function matrixStore(state = initialState, action: any) {
   switch (action.type) {
     case "GENERATE_FIELD":
       return {
-        matrix: action.matrix
+        matrix: action.matrix,
+        firstPlayerSequences: state.firstPlayerSequences,
+        secondPlayerSequences: state.secondPlayerSequences,
+        takenAmountOfCells: state.takenAmountOfCells,
+        isGameFinished: state.isGameFinished
       };
     case "TAKE_A_CELL":
       return {
-        matrix: action.matrix
+        matrix: action.matrix,
+        firstPlayerSequences: state.firstPlayerSequences,
+        secondPlayerSequences: state.secondPlayerSequences,
+        takenAmountOfCells: state.takenAmountOfCells + 1,
+        isGameFinished: state.isGameFinished
+      };
+    case "FILL_UP_SEQUENCES":
+      return {
+        matrix: state.matrix,
+        firstPlayerSequences: action.firstPlayerSequences,
+        secondPlayerSequences: action.secondPlayerSequences,
+        takenAmountOfCells: state.takenAmountOfCells,
+        isGameFinished: state.isGameFinished
+      };
+    case "GAME_OVER":
+      return {
+        isGameFinished: !state.isGameFinished
       };
 
     default:
--- src/services/GamePlayHandler.ts
+++ src/services/GamePlayHandler.ts
@@ -1,7 +1,78 @@
 import { ICell } from "../interfaces/Matrix";
 
 export default class GamePlayHandler {
-  findWinner() {}
+  amountOfCells: number = 0;
+
+  calculateWinner(
+    matrix: Array<Array<ICell>>,
+    isFirstPlayer: boolean,
+    hPointer: number,
+    vPointer: number
+  ): boolean {
+    return true;
+  }
+
+  calculateSequences(
+    matrix: Array<Array<ICell>>,
+    isFirstPlayer: boolean,
+    hPointer: number,
+    vPointer: number,
+    firstPlayerSequences: any[], //TODO fix any
+    secondPlayerSequences: any[] //TODO fix any
+  ) {
+    let sequenceSet = new Set<string>();
+    let firstSequences = firstPlayerSequences;
+    let secondSequences = secondPlayerSequences;
+
+    if (isFirstPlayer && firstSequences.length === 0) {
+      sequenceSet.add(`${hPointer},${vPointer}`);
+      firstSequences.push(sequenceSet);
+    } else if (!isFirstPlayer && secondSequences.length === 0) {
+      sequenceSet.add(`${hPointer},${vPointer}`);
+      secondSequences.push(sequenceSet);
+    }
+
+    console.log(32, this.findNearbyCells(matrix, hPointer, vPointer));
+
+    return {
+      firstSequences,
+      secondSequences
+    };
+  }
+
+  findNearbyCells(
+    matrix: Array<Array<ICell>>,
+    hPointer: number,
+    vPointer: number
+    // isFirstPlayer: boolean
+  ) {
+    let cellLeft = null;
+    let cellRight = null;
+    let cellBottom = null;
+    let cellTop = null;
+
+    //find nearby cells by horizontal
+    if (hPointer <= matrix.length - 1 && hPointer > 0) {
+      cellTop = matrix[hPointer - 1][vPointer];
+    } else if (hPointer < matrix.length - 1 && hPointer >= 0) {
+      cellBottom = matrix[hPointer + 1][vPointer];
+    }
+
+    //find nearby cells by vertical
+    if (vPointer <= matrix.length - 1 && vPointer > 0) {
+      cellLeft = matrix[hPointer][vPointer - 1];
+    } else if (vPointer < matrix.length - 1 && vPointer >= 0) {
+      cellRight = matrix[hPointer][vPointer + 1];
+    }
+
+    return { cellLeft, cellRight, cellBottom, cellTop };
+  }
+
+  calculateFinishGame(foundation: number, takenAmount: number): boolean {
+    this.amountOfCells = foundation * foundation;
+    console.log(15, this.amountOfCells - takenAmount);
+    return this.amountOfCells - takenAmount > 0 ? true : false;
+  }
 
   takeCell(
     matrix: Array<Array<ICell>>,
@@ -12,6 +83,7 @@ export default class GamePlayHandler {
     if (!matrix[hPointer][vPointer].isCellTaken) {
       matrix[hPointer][vPointer].isCellTaken = true;
       matrix[hPointer][vPointer].takenBy = isFirstPlayer ? 1 : 2;
+      this.calculateWinner(matrix, isFirstPlayer, hPointer, vPointer);
     }
 
     return matrix;
